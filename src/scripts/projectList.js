c99359bf28f53e251ac27f66993317ac50c9c0cd
stban2703 <s.t.ban1401@gmail.com>
2021-11-03 23:18:38 -0500

    First project readey

--- src/scripts/projectList.js
+++ src/scripts/projectList.js
@@ -41,8 +41,42 @@ const projectsList = [
 const solidarPage = 
 `
 <section class="gallery__view">
-    <img src="./public/projects/solidar/solidar00.gif" alt="">
-    <img src="./public/projects/solidar/solidar01.jpg" alt="">
+    <div class="gallery__image">
+        <img class="animate__animated animate__fadeIn" src="./public/projects/solidar/solidar00.gif" alt="">
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar01.jpg" alt="">
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar02.jpg" alt="">
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar03.gif" alt="">
+        </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar04.jpg" alt="">
+        </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar05.jpg" alt="">
+        </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar06.jpg" alt="">
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar07.gif" alt="">
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar08.jpg" alt="">
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar09.gif" alt="">
+    </div>
+    <div class="gallery__image gallery__image--prototype">
+        <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fn1JUQh9u2bERy7caY5K1FY%2FDISE%25C3%2591O-SOLIDAR--AGUDELO-LINARES-PANESSO-VALLECILLA-VOLVERAS%3Fnode-id%3D69%253A857%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D69%253A823" allowfullscreen></iframe>
+    </div>
+    <div class="gallery__image">
+        <img src="./public/projects/solidar/solidar10.jpg" alt="">
+    </div>
 </section>
 `
 
